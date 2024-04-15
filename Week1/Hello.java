6f7b2ba8c03f0e5bb2524bec221e8520a7cad0df
sujal-sudo <np.sujal2004@gmail.com>
2024-04-15 14:39:06 +0545

    updated

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -3,5 +3,10 @@ package Week1;
 public class Hello {
     public static void main(String[] args){
         System.out.println( "Hello World!" );  //prints out the string to the console
+
+        /*Extend/Modify the above program to print two additional lines:
+
+This is my first program */
+System.out.print("This is my first program");
     }
 }
